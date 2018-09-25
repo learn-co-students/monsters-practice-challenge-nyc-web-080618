0623e7876a658d045ce88aa85338dbd828fa5370
madtab7 <madtab7@gmail.com>
2018-09-25 10:30:30 -0400

    Done.

---
+++ monsterClass.js
@@ -0,0 +1,22 @@
+class Monster {
+  constructor(monsterJsonObj){
+    this.name = monsterJsonObj.name;
+    this.age = monsterJsonObj.age;
+    this.description = monsterJsonObj.description;
+    this.id = monsterJsonObj.id;
+    Monster.all.push(this);
+  }
+
+  render(){
+    return `
+    <div id="${this.id}">
+      <h2>${this.name}</h2>
+      <h4>${this.age}</h4>
+      <p>${this.description}</p>
+    </div>
+    `
+  }
+
+}
+
+Monster.all = []
