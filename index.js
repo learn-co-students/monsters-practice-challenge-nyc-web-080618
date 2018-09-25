a13f1b786039b2975483c4cd36600caa77f19211
github username <bobjin42@gmail.com>
2018-09-25 10:14:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,52 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+let counter = 1;
 const monsterCollection = document.getElementById('monster-container');
 const pageBack = document.getElementById('back');
 const pageforward = document.getElementById('forward')
 const createFrom = document.querySelector('.add-monster-form')
-
-
-
-  fetch('http://localhost:3000/monsters/?_limit=50&_page=1')
+const fetchFn = () => {
+  fetch(`http://localhost:3000/monsters/?_limit=50&_page=${counter}`)
     .then((response)=> {return response.json()})
     .then((monsterJsonObj)=> {
+      console.log(monsterJsonObj);
       monsterCollection.innerHTML = monsterJsonObj.map((monsterObj)=>{
         let monster = new Monster(monsterObj)
         return monster.render();
       }).join("")
+    })
+}
 
-      })
 
-  let counter = 1;
+fetchFn();
+
+  // fetch('http://localhost:3000/monsters/?_limit=50&_page=1')
+  //   .then((response)=> {return response.json()})
+  //   .then((monsterJsonObj)=> {
+  //     monsterCollection.innerHTML = monsterJsonObj.map((monsterObj)=>{
+  //       let monster = new Monster(monsterObj)
+  //       return monster.render();
+  //     }).join("")
+  //
+  //     })
+
 
   pageBack.addEventListener('click', (event) => {
       counter -= 1;
-      fetch(`http://localhost:3000/monsters/?_limit=50&_page=${counter}`)
-        .then((response)=> {return response.json()})
-        .then((monsterJsonObj)=> {
-          monsterCollection.innerHTML = monsterJsonObj.map((monsterObj)=>{
-            let monster = new Monster(monsterObj)
-            return monster.render();
-          }).join("")
-      })
+      fetchFn();
+      // fetch(`http://localhost:3000/monsters/?_limit=50&_page=${counter}`)
+      //   .then((response)=> {return response.json()})
+      //   .then((monsterJsonObj)=> {
+      //     monsterCollection.innerHTML = monsterJsonObj.map((monsterObj)=>{
+      //       let monster = new Monster(monsterObj)
+      //       return monster.render();
+      //     }).join("")
+      // })
     })
 
     pageforward.addEventListener('click', (event) => {
-      monsterCollection.innerHTML = null
         counter += 1;
-        fetch(`http://localhost:3000/monsters/?_limit=50&_page=${counter}`)
-          .then((response)=> {return response.json()})
-          .then((monsterJsonObj)=> {
-            monsterCollection.innerHTML = monsterJsonObj.map((monsterObj)=>{
-              let monster = new Monster(monsterObj)
-              return monster.render();
-              /*const allMonstersHTMLString = Monster.all.map((monster)=>{
-                return monster.render()
-              }).join('')*/
-              //monsterCollection.innerHTML = allMonstersHTMLString
-              // const pageNumber = (monsterJsonObj[49].id) / 50;
-            }).join("")
-          })
+        fetchFn();
+        // fetch(`http://localhost:3000/monsters/?_limit=50&_page=${counter}`)
+        //   .then((response)=> {return response.json()})
+        //   .then((monsterJsonObj)=> {
+        //     monsterCollection.innerHTML = monsterJsonObj.map((monsterObj)=>{
+        //       let monster = new Monster(monsterObj)
+        //       return monster.render();
+        //       /*const allMonstersHTMLString = Monster.all.map((monster)=>{
+        //         return monster.render()
+        //       }).join('')*/
+        //       //monsterCollection.innerHTML = allMonstersHTMLString
+        //       // const pageNumber = (monsterJsonObj[49].id) / 50;
+        //     }).join("")
+        //   })
       })
 
       createFrom.addEventListener('submit', (event) => {
